d61e9a9415f5a4641dabb0dd745f63c2bad5a01f
Pistachopatata <152323048+Pistachopatata@users.noreply.github.com>
2023-11-28 19:49:44 +0100

    Update Popote.cs

--- Popote.cs
+++ Popote.cs
@@ -6,13 +6,14 @@ public class Popote : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
-    {
+        {Destroy(gameObject);
+
         print("helloworld");
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {print("crickets");
         
     }
 }
